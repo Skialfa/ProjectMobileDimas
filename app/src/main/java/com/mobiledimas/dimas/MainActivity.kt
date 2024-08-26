52eb2b93bccdb3613f6cf08679a136d4d8218037
dimas <dimas.adityaramadhan071104@gmail.com>
2024-08-26 13:48:48 +0700

    Initial commit

---
+++ app/src/main/java/com/mobiledimas/dimas/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.mobiledimas.dimas
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var login_Btn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        login_Btn = findViewById(R.id.login_btn)
+
+        login_Btn.setOnClickListener{
+            val Intent = Intent(this,MainActivity2::class.java)
+            startActivity(Intent)
+        }
+    }
+}
